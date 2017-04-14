d139c5f1ecfe49f4136826fc31fb9fb238c45f31
Condzi <conradkubacki@gmail.com>
2017-04-14 09:28:08 +0200

    Added States managing

---
+++ source/states/State.cpp
@@ -0,0 +1,37 @@
+/*
+	Conrad 'Condzi' Kubacki 2017
+	https://github.com/condzi
+*/
+
+#include "State.hpp"
+
+namespace pi
+{
+	void State::StartThread()
+	{
+		this->lockWindowContext();
+		this->threadRunning = true;
+
+		this->thread = std::thread( &State::ThreadMethod, this );
+	}
+
+	void State::EndThread()
+	{
+		this->threadRunning = false;
+		this->thread.join();
+
+		this->unlockWindowContext();
+	}
+
+	void State::ThreadMethod()
+	{
+		this->unlockWindowContext();
+
+		while ( this->threadRunning )
+		{
+			this->UpdateThread( 1 / 30.0f, *this->windowPtr );
+		}
+
+		this->lockWindowContext();
+	}
+}
---
+++ source/states/States.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace pg
+{
+	// Game state enumerations
+	enum class state_t
+	{
+		EXIT = 
+		INIT = 0,
+		MENU,
+		PLAY
+	};
+}
