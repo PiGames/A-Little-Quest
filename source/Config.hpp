6e7418b6f53d6f279487b3740d1d16a97a0c4ee0
Condzi <conradkubacki@gmail.com>
2017-04-14 11:44:39 +0200

    Added Settings class

--- source/Config.hpp
+++ source/Config.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "fileManaging/INIparser.hpp"
 
 namespace constants
 {
@@ -10,6 +11,43 @@ namespace constants
 
 	namespace cell
 	{
-		constexpr auto 
 	}
+}
+
+namespace pg
+{
+	/*
+	===============================================================================
+	Created by: Condzi
+		Simple extension for pi::INIFile class. To get settings simple use Get methods.
+
+	===============================================================================
+	*/
+	class Settings final :
+		public pi::INIFile
+	{
+	public:
+		~Settings()
+		{
+			delete Settings::instance;
+		}
+
+		static Settings& GetInstance()
+		{
+			if ( !Settings::instance )
+				Settings::instance = new Settings();
+			return *Settings::instance;
+		}
+
+		void GenerateDefault( const std::string& path )
+		{
+			this->AddString( "WINDOW", "TITLE", "Platform Game" );
+			this->AddInt( "WINDOW", "FPS", 64 );
+
+			this->SaveToFile( path, "; Default config. Don't change if you don't know what are you doing!" );
+		}
+		
+	private:
+		static Settings* instance;
+	};
 }
